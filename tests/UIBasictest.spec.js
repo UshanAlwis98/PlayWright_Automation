dd91a3e7a412f2d6c51bd6476b1a3c9457cea28e
ushanAlwis98 <ushanloshitha@gmail.com>
2026-04-22 06:57:41 +0530

    Invalid login textcontent extracted.

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,11 +1,15 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright Test", async ({ browser }) => {
+test.only("Browser Context Playwright Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
-  const url = "https://online.codl.lk/login/index.php";
-  const userName = await page.goto(url);
-  console.log("Page title:", await page.title());
+  const url = await page.goto("https://online.codl.lk/login/index.php");
+  const username = page.locator("#username");
+  const password = page.locator("#password");
+  await username.fill("your_username");
+  await password.fill("your_password");
+  await page.locator("#loginbtn").click();
+  console.log(await page.locator(".alert-danger").textContent());
 });
 
 test("Browser Context Playwright Test 2", async ({ page }) => {
