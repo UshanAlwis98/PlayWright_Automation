5627adcf8d845f48df8777a6088f4b27b89a8325
ushanAlwis98 <ushanloshitha@gmail.com>
2026-08-08 00:26:26 +0530

    Invalid Credentials Login

--- tests/Clientapp.spec.js
+++ tests/Clientapp.spec.js
@@ -1,8 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test("CODL Test", async ({ browser }) => {
+test("Rahul Shetty Academy Login Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  await page.goto("https://online.codl.lk/login/index.php");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  // Login Scenario Happy Path
+  await page.fill('input[name="username"]', "testuser");
+  await page.fill('input[name="password"]', "testpassword");
+  await page.click('input[type="submit"]');
+  // console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 });
