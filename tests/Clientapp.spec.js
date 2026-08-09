a93f6edc84f54763604f3ee4609e884733f4decd
ushanAlwis98 <ushanloshitha@gmail.com>
2026-08-09 17:18:02 +0530

    Grab items.

--- tests/Clientapp.spec.js
+++ tests/Clientapp.spec.js
@@ -4,12 +4,26 @@ test("Rahul Shetty Academy Login Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
+  const Username = page.locator('input[name="username"]');
+  const Password = page.locator('input[name="password"]');
+  const SignInButton = page.locator('input[type="submit"]');
+
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
   // Login Scenario Happy Path
-  await page.fill('input[name="username"]', "testuser");
-  await page.fill('input[name="password"]', "testpassword");
-  await page.click('input[type="submit"]');
-  // console.log(await page.locator("[style*='block']").textContent());
+  await Username.fill("rahulshettyacademy");
+  await Password.fill("testpassword");
+  await SignInButton.click();
+
+  // Validate Error Message
+  console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+  await Username.fill("");
+  await Username.fill("rahulshettyacademy");
+  await Password.fill("");
+  await Password.fill("Learning@830$3mK2");
+  await SignInButton.click();
+
+  console.log(await page.locator("div.card-body a").nth(0).textContent());
 });
